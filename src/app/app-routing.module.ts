f8e9ad38965989cc4aeddfaee78fbb3a820f5866
Parth Waghmare <waghmareparth11@gmail.com>
2019-03-16 04:14:20 +0530

    parth form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'form', loadChildren: './form/form.module#FormPageModule' },
+
   
 ];
 
