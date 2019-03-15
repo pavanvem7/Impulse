b94cd068bd079f257ee194304c7968a6588e084a
Akshay Waghe <akshaywaghe1999@gmail.com>
2019-03-15 19:37:29 +0530

    akshay ka tabs 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'hospital', loadChildren: './hospital/hospital.module#HospitalPageModule' },
+  { path: 'hospital', loadChildren: './tab1/hospital/hospital.module#HospitalPageModule' }
+
 ];
 @NgModule({
   imports: [
