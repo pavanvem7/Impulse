0f7360be728152e76302de394083550b52cff311
Akshay Waghe <akshaywaghe1999@gmail.com>
2019-03-16 00:03:32 +0530

    akshay ka tab1, application, history

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'disease', loadChildren: './disease/disease.module#DiseasePageModule' },
   
 ];
 
